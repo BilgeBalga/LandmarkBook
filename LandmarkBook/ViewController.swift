d08f3c0e61f893c94ecde7b69e34890806a7195a
Bilge Balga <bilbalga@gmail.com>
2022-07-21 00:04:49 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Bilge Balga on 21.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
